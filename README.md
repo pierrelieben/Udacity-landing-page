6da9386df83a8afe773c312bf9b050293062d483
Pierre Liebenberg <pierreliebenberg@Pierres-MacBook-Air.local>
2023-02-11 13:16:38 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# Landing Page Project
+# Udacity's Landing Page Project
 
-## Table of Contents
 
-* [Instructions](#instructions)
+## Description
+The purpose of this project is to learn the fundamentals of interacting and manipulating the DOM using JavaScript, by building an interactive Landing Page from some boilerplate HTML and CSS.
 
-## Instructions
+The citeria is:
+1. Dynamically create a navbar based on the amount of sections. 
+2. Link active state CSS to the section most prominent in the viewport. 
+3. The active state must light up the relevant section in the navbar as well.
+4. Clicking on a section must scroll it into view.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+## Resources
+Basic documentation on MDN and W3Schools were used to learn throughout this project.
+1. https://www.w3schools.com/
+2. https://developer.mozilla.org/en-US/docs/
 
-To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
--- js/app.js
+++ js/app.js
@@ -23,6 +23,13 @@
  * 
 */
 
+//building nav
+let sections = document.querySelectorAll('section');
+let navbarUL = document.getElementById('navbar__list');
+let navList = '';
+
+//for active states
+const navButns = document.querySelectorAll(".menu__link")
 
 /**
  * End Global Variables
@@ -30,7 +37,12 @@
  * 
 */
 
-
+function addClass(section) {
+    section.classList.add("your-active-class");
+}
+function removeClass(section) {
+    section.classList.remove("your-active-class");
+}
 
 /**
  * End Helper Functions
@@ -39,13 +51,33 @@
 */
 
 // build the nav
+function createNavBar() {
+    sections.forEach((section) => {
+        navList += `<li> <a class="navbar__menu menu__link" href="#${section.id}">${section.dataset.nav}</a></li>`;
+        // console.log(navList);
+        
+    });
 
+    navbarUL.innerHTML = navList;
+}
 
 // Add class 'active' to section when near top of viewport
-
+function activeState() {
+    sections.forEach((section, i) => {
+        let rect = section.getBoundingClientRect();
+        let screenHeight = screen.height;
+        if (rect.top/screenHeight < 0.4 && rect.bottom/screenHeight >= 0.4) {
+            addClass(section);
+            console.log(section);
+            navButns[i].classList.add("active__button"); 
+        } else {
+            removeClass(section);
+            navButns[i].classList.remove("active__button");
+        }
+    });
+}
 
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
@@ -54,9 +86,11 @@
 */
 
 // Build menu 
+createNavBar();
 
 // Scroll to section on link click
 
 // Set sections as active
+document.addEventListener('scroll', activeState);
 
 
